421dba4d168ab99f5a7bafc63a364890f090cb61
Stamp <weerayut.naranrak@gmail.com>
2022-09-19 14:35:04 +0700

    add swagger

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,6 +1,11 @@
 import { IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'frist name',
+    example: 'Weerayut',
+  })
   @IsNotEmpty()
   firstName: string;
 
